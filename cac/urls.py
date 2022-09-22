188d1a08b3aa01bfc5cb47a27b5f1acf523eeb5a
nfnakamura <nfn1991@hotmail.com>
2022-09-22 11:26:20 -0300

    agrego más path a urls y funciones a views

--- cac/urls.py
+++ cac/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('hola_mundo', views.hola_mundo),
+    path('saludar', views.saludar,name="saludar_por_defecto"),
+    path('saludar/<str:nombre>', views.saludar,name="saludar"),
+    path('proyectos/<int:anio>/<int:mes>', views.ver_proyectos,name="proyectos")
 ]
